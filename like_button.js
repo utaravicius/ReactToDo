a7d7dbbd7a8628967ae78084e4c4f893dec47e79
fierce-bastion-87108 <justaz23@gmail.com>
2018-11-29 10:42:40 +0200

    First commit. TO-DO app example

--- like_button.js
+++ like_button.js
@@ -1,25 +1,74 @@
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
+class TodoApp extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { liked: false };
+        this.state = { items: [], text: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
-
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
+        return React.createElement(
+            "div",
+            null,
+            React.createElement(
+                "h3",
+                null,
+                "TODO"
+            ),
+            React.createElement(TodoList, { items: this.state.items }),
+            React.createElement(
+                "form",
+                { onSubmit: this.handleSubmit },
+                React.createElement(
+                    "label",
+                    { htmlFor: "new-todo" },
+                    "What needs to be done?"
+                ),
+                React.createElement("input", {
+                    id: "new-todo",
+                    onChange: this.handleChange,
+                    value: this.state.text
+                }),
+                React.createElement(
+                    "button",
+                    null,
+                    "Add #",
+                    this.state.items.length + 1
+                )
+            )
         );
+    }
+
+    handleChange(e) {
+        this.setState({ text: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (!this.state.text.length) {
+            return;
+        }
+        const newItem = {
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }));
     }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+class TodoList extends React.Component {
+    render() {
+        return React.createElement(
+            "ul",
+            null,
+            this.props.items.map(item => React.createElement(
+                "li",
+                { key: item.id },
+                item.text
+            ))
+        );
+    }
+}
